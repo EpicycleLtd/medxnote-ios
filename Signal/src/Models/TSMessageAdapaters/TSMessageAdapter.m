9301a36b23316c9d92d8cbfce0aa7a5e395ac6fb
Matthew Chen <charlesmchen@gmail.com>
2017-05-23 13:23:38 -0400

    Fix caching of message bubble sizes.
    // FREEBIE

--- Signal/src/Models/TSMessageAdapaters/TSMessageAdapter.m
+++ Signal/src/Models/TSMessageAdapaters/TSMessageAdapter.m
@@ -60,7 +60,7 @@ NS_ASSUME_NONNULL_BEGIN
 @property (nonatomic) NSDate *messageDate;
 @property (nonatomic) NSString *messageBody;
 
-@property NSUInteger identifier;
+@property (nonatomic) NSString *interactionUniqueId;
 
 @end
 
@@ -77,9 +77,8 @@ NS_ASSUME_NONNULL_BEGIN
 
     _interaction = interaction;
     _messageDate = interaction.date;
-    // TODO casting a string to an integer? At least need a comment here explaining why we are doing this.
-    // Can we just remove this? Haven't found where we're using it...
-    _identifier = (NSUInteger)interaction.uniqueId;
+
+    self.interactionUniqueId = interaction.uniqueId;
 
     if ([interaction isKindOfClass:[TSMessage class]]) {
         TSMessage *message = (TSMessage *)interaction;
@@ -383,11 +382,9 @@ NS_ASSUME_NONNULL_BEGIN
 
 - (NSUInteger)messageHash
 {
-    if (self.isMediaMessage) {
-        return [self.mediaItem mediaHash];
-    } else {
-        return self.identifier;
-    }
+    OWSAssert(self.interactionUniqueId);
+
+    return self.interactionUniqueId.hash;
 }
 
 - (NSInteger)messageState {
