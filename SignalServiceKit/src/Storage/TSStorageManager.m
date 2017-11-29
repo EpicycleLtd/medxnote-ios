d3095c221087471d97b819285dfa875f70b7b528
Matthew Chen <charlesmchen@gmail.com>
2017-11-29 11:44:58 -0500

    Register CrossProcessNotifier.

--- SignalServiceKit/src/Storage/TSStorageManager.m
+++ SignalServiceKit/src/Storage/TSStorageManager.m
@@ -302,6 +302,7 @@ void setDatabaseInitialized()
 - (void)setupDatabaseWithSafeBlockingMigrations:(void (^_Nonnull)())safeBlockingMigrationsBlock
 {
     // Synchronously register extensions which are essential for views.
+    [TSDatabaseView registerCrossProcessNotifier];
     [TSDatabaseView registerThreadInteractionsDatabaseView];
     [TSDatabaseView registerThreadDatabaseView];
     [TSDatabaseView registerUnreadDatabaseView];
