fe813ccc4dd398b65c229202bb22a6b7c8d9643a
Jan Nemeček <jnemecek@razor.rs>
2018-06-27 15:55:22 +0200

    Add PDFViewer for PDF attachments instead of sharing

---
+++ Signal/src/ViewControllers/PDFViewer/PDFViewerViewController.h
@@ -0,0 +1,15 @@
+//
+//  PDFViewerViewController.h
+//  Medxnote
+//
+//  Created by Jan Nemecek on 27/6/18.
+//  Copyright © 2018 Open Whisper Systems. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PDFViewerViewController : UIViewController
+
+@property (nonatomic, strong) NSURL *url;
+
+@end
